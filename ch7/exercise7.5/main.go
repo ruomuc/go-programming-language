61f3ac47bcfa97242a900f8bf1123c7972377415
ruomu <252615299@qq.com>
2022-02-20 12:39:07 +0800

    练习题 7.5

--- ch7/exercise7.5/main.go
+++ ch7/exercise7.5/main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
-	"exercise7.5/tempconv"
-	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
-// 练习7.6：在 tempflag 中支持热力学温度。
-var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
+// 练习7.5：io 包中的 LimitReader 函数接受 io.Reader r 和字节数 n，
+// 返回一个 Reader，该返回值从 r 读取数据，但在读取 n 字节后报告文件结束。
+// 请实现该函数。
+// func LimitReader(r io.Reader, n int64) io.Reader
 
 func main() {
-	flag.Parse()
-	fmt.Println(*temp)
+	lr := LimitReader(strings.NewReader("qwert"), 1)
+	b, err := ioutil.ReadAll(lr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s", b)
+}
+
+type limitReader struct {
+	r          io.Reader
+	limitBytes int64
+}
+
+func (lr *limitReader) Read(p []byte) (n int, err error) {
+	if lr.limitBytes <= 0 {
+		return 0, io.EOF
+	}
+	if int64(len(p)) > lr.limitBytes {
+		p = p[:lr.limitBytes]
+	}
+	n, err = lr.r.Read(p)
+	lr.limitBytes -= int64(n)
+	return
+}
+
+func LimitReader(r io.Reader, n int64) io.Reader {
+	lr := &limitReader{
+		r:          r,
+		limitBytes: n,
+	}
+	return lr
 }
---
+++ ch7/exercise7.8/main.go
@@ -0,0 +1,6 @@
+package main
+
+// 练习7.8：
+func main() {
+
+}
