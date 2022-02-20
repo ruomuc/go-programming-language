5de625147f6d324d9f10007edbe59e47cf823f9a
ruomu <252615299@qq.com>
2022-02-20 20:43:46 +0800

    练习题 7.9

--- ch7/exercise7.9/multi-sort/sort.go
+++ ch7/exercise7.9/multi-sort/sort.go
@@ -2,54 +2,52 @@ package multi_sort
 
 import (
 	"fmt"
-	"os"
 	"sort"
-	"text/tabwriter"
 	"time"
 )
 
-var tracks = []*Track{
-	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
-	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
-	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
-	{"Go", "Moby", "Moby", 1992, length("3m37s")},
-	{"Go", "Moby2", "Moby2", 1992, length("3m36s")},
-}
-
-func Click(m *multiSort, sortField string) {
-	m.sortOrder = append(m.sortOrder, sortField)
+func Click(m *MultiSort, sortField string) {
+	var exist bool
+	for _, so := range m.SortOrder {
+		if so == sortField {
+			exist = true
+		}
+	}
+	if !exist {
+		m.SortOrder = append(m.SortOrder, sortField)
+	}
 	sort.Sort(m)
 }
 
-type multiSort struct {
-	t         []*Track
-	sortOrder []string
+type MultiSort struct {
+	T         []*Track
+	SortOrder []string
 }
 
-func (m multiSort) Len() int { return len(m.t) }
+func (m MultiSort) Len() int { return len(m.T) }
 
-func (m multiSort) Less(i, j int) bool {
-	for _, st := range m.sortOrder {
+func (m MultiSort) Less(i, j int) bool {
+	for _, st := range m.SortOrder {
 		switch st {
 		case "title":
-			if m.t[i].Title != m.t[j].Title {
-				return m.t[i].Title < m.t[j].Title
+			if m.T[i].Title != m.T[j].Title {
+				return m.T[i].Title < m.T[j].Title
 			}
 		case "artist":
-			if m.t[i].Artist != m.t[j].Artist {
-				return m.t[i].Artist < m.t[j].Artist
+			if m.T[i].Artist != m.T[j].Artist {
+				return m.T[i].Artist < m.T[j].Artist
 			}
 		case "album":
-			if m.t[i].Album != m.t[j].Album {
-				return m.t[i].Album < m.t[j].Album
+			if m.T[i].Album != m.T[j].Album {
+				return m.T[i].Album < m.T[j].Album
 			}
 		case "year":
-			if m.t[i].Year != m.t[j].Year {
-				return m.t[i].Year < m.t[j].Year
+			if m.T[i].Year != m.T[j].Year {
+				return m.T[i].Year < m.T[j].Year
 			}
 		case "length":
-			if m.t[i].Length != m.t[j].Length {
-				return m.t[i].Length < m.t[j].Length
+			if m.T[i].Length != m.T[j].Length {
+				return m.T[i].Length < m.T[j].Length
 			}
 		default:
 			panic(fmt.Sprintf("invalid case value %s", st))
@@ -58,7 +56,7 @@ func (m multiSort) Less(i, j int) bool {
 	return false
 }
 
-func (m multiSort) Swap(i, j int) { m.t[i], m.t[j] = m.t[j], m.t[i] }
+func (m MultiSort) Swap(i, j int) { m.T[i], m.T[j] = m.T[j], m.T[i] }
 
 type Track struct {
 	Title  string
@@ -68,22 +66,10 @@ type Track struct {
 	Length time.Duration
 }
 
-func length(s string) time.Duration {
+func Length(s string) time.Duration {
 	d, err := time.ParseDuration(s)
 	if err != nil {
 		panic(s)
 	}
 	return d
-}
-
-func printTracks(tracks []*Track) {
-	const format = "%v\t%v\t%v\t%v\t%v\t\n"
-	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
-	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
-	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
-	for _, t := range tracks {
-		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
-	}
-	tw.Flush() // calculate column widths and print table
-	fmt.Println()
 }
