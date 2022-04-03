c25f3aea342653c4be2c359e71f0b22e16e62555
ruomu <252615299@qq.com>
2022-04-03 21:15:08 +0800

    exercise9.3

--- ch9/exercise9.3/main.go
+++ ch9/exercise9.3/main.go
@@ -1,7 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"memo/memo"
+	"net/http"
+	"time"
+)
+
 // 练习 9.3： 扩展Func类型和(*Memo).Get方法，支持调用方提供一个可选的done channel，
 // 使其具备通 过该channel来取消整个操作的能力(§8.9)。一个被取消了的Func的调用结果不应该被缓存。
-func main() {
+const timeout = 5 * time.Minute
 
+func main() {
+	m := memo.New(httpGetBody)
+	d := make(chan struct{})
+	go func() {
+		time.Sleep(timeout)
+		close(d)
+	}()
+	data, err := m.Get("https://www.baidu.com", d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(data)
+}
+
+func httpGetBody(url string, done <-chan struct{}) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
--- ch9/exercise9.4/main_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestPipeline(t *testing.T) {
-
-}
